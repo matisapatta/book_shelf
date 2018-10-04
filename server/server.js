ce653c1e35d912dc2498233e9ba337274210e253
matisapatta <mati.sapatta@gmail.com>
2018-10-03 23:36:00 -0300

    commi

---
+++ server/server.js
@@ -0,0 +1,24 @@
+/**
+ * Start require libs
+ */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config  = require('./config/config').get(process.env.NODE_ENV)
+/**
+ * End require libs
+ */
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE)
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{
+    console.log("Server running on port",port)
+})
+app.use(bodyParser.json());
+app.use(cookieParser())
